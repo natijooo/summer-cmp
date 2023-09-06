69cafa314a2ffaad6d2f32837f7fb941d234b00d
natijooo <128637086+natijooo@users.noreply.github.com>
2023-09-06 16:10:39 +0300

    task.sh

---
+++ task.sh
@@ -0,0 +1,12 @@
+#updating and upgrading
+sudo apt-get update && upgrade -y
+#Scanning ports and vulnurabilities
+sudo apt install nmap -y
+# Install directory and file brute-forcing tools
+sudo apt install gobuster -y
+sudo apt install ffuf -y
+# Install Metasploit Framework
+sudo apt install metasploit-framework -y
+#ip address scanner 
+sudo apt install nslookup
+
